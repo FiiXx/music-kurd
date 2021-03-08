f36e9cafaf39d141454d7b979f4da4a3e5d85fbf
REDZONE489 <78139545+REDZONE489@users.noreply.github.com>
2021-03-08 16:22:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ fs.readdir(__dirname + "/events/", (err, files) => {
   if (err) return console.error(err);
   files.forEach((file) => {
     const event = require(__dirname + `/events/${file}`);
-    let eventName = file.split(".")[0];
+    let eventName = file.split("$")[0];
     client.on(eventName, event.bind(null, client));
     console.log("Loading Event: "+eventName)
   });
@@ -28,11 +28,11 @@ fs.readdir("./commands/", (err, files) => {
   files.forEach((file) => {
     if (!file.endsWith(".js")) return;
     let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
+    let commandName = file.split("$")[0];
     client.commands.set(commandName, props);
     console.log("Loading Command: "+commandName)
   });
 });
 
 //Logging in to discord
-client.login(process.env.TOKEN)
+client.login(process.env.ODE4NDY4ODM0MDIxMDgxMDk4.YEYggA._xwpd2HGiLi1xEK64wgd_JMuO2k)
